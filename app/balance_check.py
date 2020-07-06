e7a766763f914949afcb3c3eeb2505d6885d7780
Nishant <nishant@teamygm.in>
2020-07-06 01:55:23 -0700

    Working on monthly planner with atm balance condition.

--- app/balance_check.py
+++ app/balance_check.py
@@ -7,18 +7,17 @@ mydb = mysql.connector.connect(
                     passwd = "Password@123",
                     auth_plugin = "mysql_native_password",)
 
-mycur = mydb.cursor()
+mycur = mydb.cursor(buffered=True)
 
 min_balance = 50000
+
 
 mycur.execute("use YGMDB")
 mycur.execute("select count(*) from Dummy")
 count = mycur.fetchone()
 mycur.execute(f"create temporary table temp_tbl_balance(select Station_ID,Total_Cash,File_Date,File_Time from Hopper_File_History order by Hopper_File_History.File_Date desc, Hopper_File_History.File_Time desc limit {count[0]})")
-#mycur.execute(f"select Station_ID,Total_Cash from temp_tbl_balance where Total_Cash <={min_balance};")
-mycur.execute("select * from temp_tbl")
+mycur.execute(f"select Station_ID,Total_Cash from temp_tbl_balance where Total_Cash <={min_balance};")
+mycur.execute("select * from temp_tbl_balance")
 temp = mycur.fetchall()
 for i in temp:
     print(i)
-
-
